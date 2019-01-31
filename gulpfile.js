e38967892a416755078030e12aca82715d2b4dd5
SukkaW <isukkaw@gmail.com>
2019-01-31 18:51:31 +0800

    chore: update build

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,34 @@
 const gulp = require('gulp');
 const yml = require('gulp-yaml');
+const htmlmin = require("gulp-htmlmin");
+const htmlclean = require("gulp-htmlclean");
 
-gulp.task('build', () => {
+let yml2json = () => {
     return gulp.src('./src/cdn.yml')
         .pipe(yml({
             schema: 'DEFAULT_SAFE_SCHEMA',
             space: 2,
         }))
-        .pipe(gulp.dest('./dist/'))
-});
+        .pipe(gulp.dest('./dist/'));
+}
+
+let minifyHTML = () => {
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true,
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('dist'));
+}
+
+exports.minifyHTML = minifyHTML;
+exports.yml2json = yml2json;
+
+gulp.task('build', gulp.series(
+    yml2json,
+    minifyHTML
+));
 
 gulp.task('default', gulp.parallel('build'));
